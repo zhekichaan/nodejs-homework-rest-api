d2db3092c354016ab5a441711d4017c29ac5192e
zhekichaan <eugene.chernitskiy@gmail.com>
2022-11-09 18:53:22 -0500

    update

--- app.js
+++ app.js
@@ -14,26 +14,11 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-// app.use((req, res) => {
-//   console.log(req);
-//   res.status(404).json({ message: 'Not found' })
-// })
-
 app.use((_, res) => res.status(404).json({ message: "Not Found" }));
 
 app.use((err, _, res, __) => {
   const { status = 500, message = "Server internal error" } = err;
   res.status(status).json({ message });
 });
-
-// app.use((err, req, res, next) => {
-//   console.log(err);
-//   res.status(500).json({ message: err.message })
-// })
-
-// app.use((err, _, res, __) => {
-//   const { status = 500, message = "Server internal error" } = err;
-//   res.status(status).json({ message });
-// });
 
 module.exports = app
--- middlewares/validationBody.js
+++ middlewares/validationBody.js
@@ -1,7 +1,6 @@
 const validationBody = (schema) => {
     return function(req, res, next) {
         const { error } = schema.validate(req.body);
-        console.log(error);
         next(error)
     }
 }
