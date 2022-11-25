b365134c3cecab74a2895dea5e28de71d6dd56b3
zhekichaan <eugene.chernitskiy@gmail.com>
2022-11-24 20:04:53 -0500

    fix

--- router/user.router.js
+++ router/user.router.js
@@ -6,12 +6,10 @@ const { auth } = require('../middlewares/auth');
 const { validationBody } = require('../middlewares/validationBody');
 const { authSchema, subscriptionSchema } = require('../schemas/schema');
 
-
 router.post("/signup", validationBody(authSchema), tryCatchWrapper(ctrlUser.register));
 router.post("/login", validationBody(authSchema), tryCatchWrapper(ctrlUser.login));
 router.post("/logout", tryCatchWrapper(auth), tryCatchWrapper(ctrlUser.logout));
 router.post("/current", tryCatchWrapper(auth), tryCatchWrapper(ctrlUser.current));
 router.patch("/", tryCatchWrapper(auth), validationBody(subscriptionSchema), tryCatchWrapper(ctrlUser.updateSubscription));
-
 
 module.exports = router;
